8f53965b3a2bd083ef79d72c12cbbf570bdb7a40
Yonatan Israeli <yonirisraeli@gmail.com>
2024-04-20 21:17:52 -0400

    Update Windows User Profile Transfer Tool.bat

--- Windows User Profile Transfer Tool.bat
+++ Windows User Profile Transfer Tool.bat
@@ -2,7 +2,7 @@
 setlocal
 title Windows User Profile Transfer Tool
 echo Program Name: Windows User Profile Transfer Tool
-echo Version: 1.0.5
+echo Version: 1.0.6
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -67,7 +67,7 @@ goto Sure1
 :Copy1
 echo.
 echo Copying %UserProfileFrom% to %UserProfileTo%.
-xcopy "%SystemDrive%\Users\%UserProfileFrom%\*.*" "%SystemDrive%\Users\%UserProfileTo%" /y /e /r /h /c /q > nul 2>&1
+xcopy "%SystemDrive%\Users\%UserProfileFrom%\*.*" "%SystemDrive%\Users\%UserProfileTo%" /y /t /e /k /r /h /c /q > nul 2>&1
 if not "%errorlevel%"=="0" goto Error1
 echo.
 echo User Profile transfer complete! Press any key to continue.
@@ -100,7 +100,7 @@ if exist "%FileTo%\%UserProfileFrom% File" goto FileToFileExist
 echo.
 echo Copying %UserProfileFrom% to "%FileTo%".
 md "%FileTo%\%UserProfileFrom% File"
-xcopy "%SystemDrive%\Users\%UserProfileFrom%\*.*"  "%FileTo%\%UserProfileFrom% File" /y /e /r /h /c /q > nul 2>&1
+xcopy "%SystemDrive%\Users\%UserProfileFrom%\*.*"  "%FileTo%\%UserProfileFrom% File" /y /t /e /k /r /h /c /q > nul 2>&1
 if not "%errorlevel%"=="0" goto Error2
 echo.
 echo %UserProfileFrom% copied to "%FileTo%\%UserProfileFrom% File". Press any key to continue.
@@ -157,7 +157,7 @@ goto Sure3
 :Copy3
 echo.
 echo Copying "%File%" to %UserProfileTo%.
-xcopy "%File%\*.*" "%SystemDrive%\Users\%UserProfileTo%" /y /e /r /h /c /q > nul 2>&1
+xcopy "%File%\*.*" "%SystemDrive%\Users\%UserProfileTo%" xcopy "%SystemDrive%\Users\%UserProfileFrom%\*.*"  "%FileTo%\%UserProfileFrom% File" /y /t /e /k /r /h /c /q > nul 2>&1 > nul 2>&1
 if not "%errorlevel%"=="0" goto Error3
 echo.
 echo User Profile transfer complete! Press any key to continue.
