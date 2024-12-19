30a566f8a1ff986e1298ed79304aae64a9ae9f56
Yonatan Israeli <yonirisraeli@gmail.com>
2024-12-19 18:58:14 -0500

    Update Windows User Profile Transfer-Backup-Restore Tool.bat

--- Windows User Profile Transfer-Backup-Restore Tool.bat
+++ Windows User Profile Transfer-Backup-Restore Tool.bat
@@ -2,10 +2,11 @@
 setlocal
 title Windows User Profile Transfer/Backup/Restore Tool
 echo Program Name: Windows User Profile Transfer/Backup/Restore Tool
-echo Version: 7.0.7
-echo Developer: @YonatanReuvenIsraeli
-echo Website: https://www.yonatanreuvenisraeli.dev
+echo Version: 7.0.8
 echo License: GNU General Public License v3.0
+echo Developer: @YonatanReuvenIsraeli
+echo GitHub: https://github.com/YonatanReuvenIsraeli
+echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli 
 net session > nul 2>&1
 if not "%errorlevel%"=="0" goto "NotAdministrator"
 goto "Start"
